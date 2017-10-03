ff5324282a1e1bebb3238f3e80d2c3037abae48b
Péter Alius <peter.alius@liferay.com>
2017-10-03 15:24:38 +0200

    added multiplier

--- game.js
+++ game.js
@@ -1,18 +1,21 @@
 const player = {
-    money: 990,
-    funds: 1000
+    money: 0,
+    funds: 10,
+    multiplier: 1
 };
 
 var lastUpdate = new Date().getTime();
 var scientific = true;
 
 document.querySelectorAll("[name=shortenType]").forEach(
-    function(e) {
-        e.onclick = function() {
-            scientific = (e.value === "scientific") ? true : false;
-        };
-    }
+    e => e.onclick = () => scientific = (e.value === "scientific") ? true : false
 );
+
+document.getElementById("buyMultiplier").onclick = () => {
+    player.multiplier = (player.money / player.funds) * player.multiplier;
+    player.money = 0;
+};
+
 
 MoneyFormat = ['K', 'M', 'B', 'T', 'Qd', 'Qt', 'Sx', 'Sp', 'Oc', 'No', 'Dc', 'UDc', 'DDc', 'TDc', 'QdDc', 'QtDc', 'SxDc', 'SpDc', 'ODc', 'NDc', 'Vg', 'UVg', 'DVg', 'TVg', 'QdVg', 'QtVg', 'SxVg', 'SpVg', 'OVg', 'NVg', 'Tg', 'UTg', 'DTg', 'TTg', 'QdTg', 'QtTg', 'SxTg', 'SpTg', 'OTg','NTg', 'Qa', 'UQa', 'DQa', 'TQa', 'QdQa', 'QtQa', 'SxQa', 'SpQa', 'OQa', 'NQa', 'Qi', 'UQi', 'DQi', 'TQi', 'QaQi', 'QtQi', 'SxQi', 'SpQi', 'OQi', 'NQi', 'Se', 'USe', 'DSe', 'TSe', 'QaSe', 'QtSe', 'SxSe', 'SpSe', 'OSe', 'NSe', 'St', 'USt', 'DSt', 'TSt', 'QaSt', 'QtSt', 'SxSt', 'SpSt', 'OSt', 'NSt', 'Og', 'UOg', 'DOg', 'TOg', 'QdOg', 'QtOg', 'SxOg', 'SpOg', 'OOg', 'NOg', 'Nn', 'UNn', 'DNn', 'TNn', 'QdNn', 'QtNn', 'SxNn', 'SpNn', 'ONn', 'NNn'];
 MoneyFormat.reverse();
@@ -36,8 +39,9 @@ setInterval(function() {
     diff = diff / 100;
 
     document.getElementById("numbers").innerHTML = shorten(player.money);
+    document.getElementById("multiplier").innerHTML = shorten(player.multiplier);
 
-    player.money += player.funds*(1.2)*diff;
+    player.money += player.funds*(1 + player.multiplier*0.25)*diff/10;
 
     lastUpdate = thisUpdate;    
 }, 100);
