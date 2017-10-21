369b3b3ca95ef8dc93308eb7dd894e955d409197
Péter Alius <peter.alius@liferay.com>
2017-10-21 10:03:40 +0200

    minor changes

--- game.js
+++ game.js
@@ -10,8 +10,8 @@ const firstUpgrade = {
     value: 1
 };
 
-var lastUpdate = new Date().getTime();
-var scientific = true;
+let lastUpdate = new Date().getTime();
+let scientific = true;
 
 document.querySelectorAll("[name=shortenType]").forEach(
     e => e.onclick = () => scientific = (e.value === "scientific") ? true : false
@@ -26,13 +26,13 @@ document.getElementById("buyFirstUpgrade").onclick = () => {
 };
 
 
-MoneyFormat = ['K', 'M', 'B', 'T', 'Qd', 'Qt', 'Sx', 'Sp', 'Oc', 'No', 'Dc', 'UDc', 'DDc', 'TDc', 'QdDc', 'QtDc', 'SxDc', 'SpDc', 'ODc', 'NDc', 'Vg', 'UVg', 'DVg', 'TVg', 'QdVg', 'QtVg', 'SxVg', 'SpVg', 'OVg', 'NVg', 'Tg', 'UTg', 'DTg', 'TTg', 'QdTg', 'QtTg', 'SxTg', 'SpTg', 'OTg','NTg', 'Qa', 'UQa', 'DQa', 'TQa', 'QdQa', 'QtQa', 'SxQa', 'SpQa', 'OQa', 'NQa', 'Qi', 'UQi', 'DQi', 'TQi', 'QaQi', 'QtQi', 'SxQi', 'SpQi', 'OQi', 'NQi', 'Se', 'USe', 'DSe', 'TSe', 'QaSe', 'QtSe', 'SxSe', 'SpSe', 'OSe', 'NSe', 'St', 'USt', 'DSt', 'TSt', 'QaSt', 'QtSt', 'SxSt', 'SpSt', 'OSt', 'NSt', 'Og', 'UOg', 'DOg', 'TOg', 'QdOg', 'QtOg', 'SxOg', 'SpOg', 'OOg', 'NOg', 'Nn', 'UNn', 'DNn', 'TNn', 'QdNn', 'QtNn', 'SxNn', 'SpNn', 'ONn', 'NNn'];
+const MoneyFormat = ['K', 'M', 'B', 'T', 'Qd', 'Qt', 'Sx', 'Sp', 'Oc', 'No', 'Dc', 'UDc', 'DDc', 'TDc', 'QdDc', 'QtDc', 'SxDc', 'SpDc', 'ODc', 'NDc', 'Vg', 'UVg', 'DVg', 'TVg', 'QdVg', 'QtVg', 'SxVg', 'SpVg', 'OVg', 'NVg', 'Tg', 'UTg', 'DTg', 'TTg', 'QdTg', 'QtTg', 'SxTg', 'SpTg', 'OTg','NTg', 'Qa', 'UQa', 'DQa', 'TQa', 'QdQa', 'QtQa', 'SxQa', 'SpQa', 'OQa', 'NQa', 'Qi', 'UQi', 'DQi', 'TQi', 'QaQi', 'QtQi', 'SxQi', 'SpQi', 'OQi', 'NQi', 'Se', 'USe', 'DSe', 'TSe', 'QaSe', 'QtSe', 'SxSe', 'SpSe', 'OSe', 'NSe', 'St', 'USt', 'DSt', 'TSt', 'QaSt', 'QtSt', 'SxSt', 'SpSt', 'OSt', 'NSt', 'Og', 'UOg', 'DOg', 'TOg', 'QdOg', 'QtOg', 'SxOg', 'SpOg', 'OOg', 'NOg', 'Nn', 'UNn', 'DNn', 'TNn', 'QdNn', 'QtNn', 'SxNn', 'SpNn', 'ONn', 'NNn'];
 MoneyFormat.reverse();
 
-shorten = function(money) {
-	var temp = MoneyFormat.length;
-	var digitMul = Math.pow(10, 2);
-	for (var i = 0; i < MoneyFormat.length; i++) {
+const shorten = (money) => {
+	let temp = MoneyFormat.length;
+	let digitMul = Math.pow(10, 2);
+	for (let i = 0; i < MoneyFormat.length; i++) {
 		if ( Math.pow(10, temp * 3) <= money ) {
 			money = money / Math.pow(10, temp * 3);
 			return scientific ? money.toFixed(2) + 'e+' + (MoneyFormat.length-i)*3 : money.toFixed(2) + ' ' + MoneyFormat[i];
@@ -42,10 +42,10 @@ shorten = function(money) {
 	return money.toFixed(1);
 };
 
-shortenPrice = function(money) {
-	var temp = MoneyFormat.length;
-	var digitMul = Math.pow(10, 2);
-	for (var i = 0; i < MoneyFormat.length; i++) {
+const shortenPrice = (money) => {
+	let temp = MoneyFormat.length;
+	let digitMul = Math.pow(10, 2);
+	for (let i = 0; i < MoneyFormat.length; i++) {
 		if ( Math.pow(10, temp * 3) <= money ) {
 			money = money / Math.pow(10, temp * 3);
 			return scientific ? money.toFixed(2) + 'e+' + (MoneyFormat.length-i)*3 : money.toFixed(2) + ' ' + MoneyFormat[i];
@@ -55,10 +55,9 @@ shortenPrice = function(money) {
 	return money.toFixed(2);
 };
 
-setInterval(function() {
-    var thisUpdate = new Date().getTime();
-    var diff = thisUpdate - lastUpdate;
-    diff = diff / 100;
+setInterval(() => {
+    const thisUpdate = new Date().getTime();
+    const diff = (thisUpdate - lastUpdate) / 100;
 
     document.getElementById("numbers").innerHTML = shorten(player.money);
     document.getElementById("multiplier").innerHTML = shortenPrice(firstUpgrade.price);
